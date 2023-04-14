43c559aace7f1648f86fdef80db92e382a01d9a3
Alfie Bowman <alfiebowman@protonmail.com>
2023-04-14 16:43:50 +0100

    Add argument to function

--- src/parakeet/console.py
+++ src/parakeet/console.py
@@ -3,6 +3,8 @@ from . import __version__
 
 @click.command()
 @click.version_option(version=__version__)
-def main():
-	"""Command line Parquet tool"""
-	click.echo("Hello, world!")
+@click.argument("filename", nargs=1)
+def main(filename):
+    """Command line Parquet tool"""
+    click.echo(f"filename: {filename}")
+
