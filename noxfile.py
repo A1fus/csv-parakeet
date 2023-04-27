f9a544c2b2c4ba819f5411fe210795f883db808f
Alfie Bowman <alfiebowman@protonmail.com>
2023-04-27 13:02:03 +0100

    Add black session

--- noxfile.py
+++ noxfile.py
@@ -1,15 +1,25 @@
 import nox
 
+
 @nox.session(python=["3.9", "3.10"])
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
 
+
 locations = "src", "tests", "noxfile.py"
+
 
 @nox.session(python=["3.9", "3.10"])
 def lint(session):
     args = session.posargs or locations
     session.install("flake8")
     session.run("flake8", *args)
+
+
+@nox.session(python="3.10")
+def black(session):
+    args = session.posargs or locations
+    session.install("black")
+    session.run("black", *args)
