75a4c02a5c53487146438ae1826f9d6af48b6f62
Alfie Bowman <alfiebowman@protonmail.com>
2023-04-28 12:25:31 +0100

    Manage nox sessions via Poetry

--- noxfile.py
+++ noxfile.py
@@ -3,7 +3,20 @@ import tempfile
 import nox
 
 locations = "src", "tests", "noxfile.py"
-nox.options.sessions = "lint", "tests", "saftey"
+nox.options.sessions = "lint", "tests", "safety"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--requirement={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.9", "3.10"])
@@ -16,7 +29,7 @@ def tests(session):
 @nox.session(python=["3.9", "3.10"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-bugbear", "flake8-bandit")
+    install_with_constraints(session, "flake8", "flake8-bugbear", "flake8-bandit")
     session.run("flake8", *args)
 
 
@@ -26,25 +39,25 @@ def safety(session):
         session.run(
             "poetry",
             "export",
-            "--with dev",
+            "--dev",
             "--format=requirements.txt",
             "--without-hashes",
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
 @nox.session(python="3.10")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
 @nox.session(python="3.10")
 def isort(session):
     args = session.posargs or locations
-    session.install("isort")
+    install_with_constraints(session, "isort")
     session.run("isort", *args)
