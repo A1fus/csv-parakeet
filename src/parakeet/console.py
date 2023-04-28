4d8fd18fed6b0c585f8c9711d07c5295408f7c5d
Alfie Bowman <alfiebowman@protonmail.com>
2023-04-28 16:12:08 +0100

    Add path type to IO arguments

--- src/parakeet/console.py
+++ src/parakeet/console.py
@@ -6,8 +6,8 @@ from . import __version__
 
 @click.command()
 @click.version_option(version=__version__)
-@click.argument("file_in", nargs=1)
-@click.argument("file_out", nargs=1)
+@click.argument("file_in", type=click.Path(exists=True), nargs=1)
+@click.argument("file_out", type=click.Path(), nargs=1)
 def main(file_in, file_out):
     """Command line Parquet tool"""
     pd.read_csv(file_in).to_parquet(path=file_out, index=False)
