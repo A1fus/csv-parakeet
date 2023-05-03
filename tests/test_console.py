7e5ca1e9765b938c05d585602e20bd550722111d
Alfie Bowman <alfiebowman@protonmail.com>
2023-05-03 08:53:31 +0100

    Create test for successful writing

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,13 +4,20 @@ from pathlib import Path
 
 import click.testing
 import pandas as pd
+import pytest
 
 from parakeet import console
 
 
+@pytest.fixture
+def test_df() -> pd.DataFrame:
+    return pd.DataFrame(data={"a": [0, 1, 2]})
+
+
 def setup_function():
     os.makedirs("./testfiles", exist_ok=True)
-    pd.DataFrame(data={"a": [0, 1, 2]}).to_csv("./testfiles/test.csv")
+    input_df = pd.DataFrame(data={"a": [0, 1, 2]})
+    input_df.to_csv("./testfiles/test.csv", index=False)
 
 
 def teardown_function():
@@ -25,3 +32,11 @@ def test_c2p_succeeds():
         console.c2p, args=["testfiles/test.csv", "testfiles/test.parquet"]
     )
     assert result.exit_code == 0
+
+
+def test_output_correct(test_df):
+    runner = click.testing.CliRunner()
+    runner.invoke(console.c2p, args=["testfiles/test.csv", "testfiles/test.parquet"])
+    saved_df = pd.read_parquet("testfiles/test.parquet")
+    input_df = test_df
+    pd.testing.assert_frame_equal(saved_df, input_df)
