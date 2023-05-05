64e1a694c83246982a9db134167dc14761c472a0
Alfie Bowman <alfiebowman@protonmail.com>
2023-05-05 13:53:59 +0100

    Add p2c function

--- src/parakeet/console.py
+++ src/parakeet/console.py
@@ -16,3 +16,11 @@ def parakeet() -> None:
 def c2p(file_in, file_out):
     """Convert a CSV file to parquet format"""
     pd.read_csv(file_in).to_parquet(path=file_out, index=False)
+
+
+@parakeet.command()
+@click.argument("file_in", type=click.Path(exists=True), nargs=1)
+@click.argument("file_out", type=click.Path(), nargs=1)
+def p2c(file_in, file_out):
+    """Convert a parqet file to CSV format"""
+    pd.read_parquet(file_in).to_csv(path_or_buf=file_out, index=False)
--- tests/test_console.py
+++ tests/test_console.py
@@ -17,7 +17,8 @@ def test_df() -> pd.DataFrame:
 def setup_function():
     os.makedirs("./testfiles", exist_ok=True)
     input_df = pd.DataFrame(data={"a": [0, 1, 2]})
-    input_df.to_csv("./testfiles/test.csv", index=False)
+    input_df.to_csv("./testfiles/test_input.csv", index=False)
+    input_df.to_parquet("./testfiles/test_input.parquet", index=False)
 
 
 def teardown_function():
@@ -35,14 +36,27 @@ def test_parakeet_succeeds():
 def test_c2p_succeeds():
     runner = click.testing.CliRunner()
     result = runner.invoke(
-        console.c2p, args=["testfiles/test.csv", "testfiles/test.parquet"]
+        console.c2p,
+        args=["testfiles/test_input.csv", "testfiles/test_succeeds.parquet"],
     )
     assert result.exit_code == 0
 
 
-def test_output_correct(test_df):
+def test_c2p_output_correct(test_df):
     runner = click.testing.CliRunner()
-    runner.invoke(console.c2p, args=["testfiles/test.csv", "testfiles/test.parquet"])
-    saved_df = pd.read_parquet("testfiles/test.parquet")
+    runner.invoke(
+        console.c2p, args=["testfiles/test_input.csv", "testfiles/test_output.parquet"]
+    )
+    saved_df = pd.read_parquet("testfiles/test_output.parquet")
+    input_df = test_df
+    pd.testing.assert_frame_equal(saved_df, input_df)
+
+
+def test_p2c_output_correct(test_df):
+    runner = click.testing.CliRunner()
+    runner.invoke(
+        console.p2c, args=["testfiles/test_input.parquet", "testfiles/test_output.csv"]
+    )
+    saved_df = pd.read_csv("testfiles/test_output.csv")
     input_df = test_df
     pd.testing.assert_frame_equal(saved_df, input_df)
