34d204ee6ed735c6ff7aaabd5d0be60c683b9b50
Alfie Bowman <alfiebowman@protonmail.com>
2023-05-05 15:28:11 +0100

    Improve docstrings

--- src/parakeet/console.py
+++ src/parakeet/console.py
@@ -7,6 +7,7 @@ from . import __version__
 @click.group()
 @click.version_option(version=__version__)
 def parakeet() -> None:
+    """Tool to easily convert between CSV and parquet files"""
     return None  # pragma: no cover
 
 
@@ -14,7 +15,13 @@ def parakeet() -> None:
 @click.argument("file_in", type=click.Path(exists=True), nargs=1)
 @click.argument("file_out", type=click.Path(), nargs=1)
 def c2p(file_in, file_out):
-    """Convert a CSV file to parquet format"""
+    """Convert a CSV file to parquet format
+
+    \b
+    Arguments:
+        FILE_IN: path to a csv file
+        FILE_OUT: desired path for parquet output
+    """
     pd.read_csv(file_in).to_parquet(path=file_out, index=False)
 
 
@@ -22,5 +29,11 @@ def c2p(file_in, file_out):
 @click.argument("file_in", type=click.Path(exists=True), nargs=1)
 @click.argument("file_out", type=click.Path(), nargs=1)
 def p2c(file_in, file_out):
-    """Convert a parqet file to CSV format"""
+    """Convert a parqet file to CSV format
+
+    \b
+    Arguments:
+        FILE_IN: path to a parquet file
+        FILE_OUT: desired path for csv output
+    """
     pd.read_parquet(file_in).to_csv(path_or_buf=file_out, index=False)
