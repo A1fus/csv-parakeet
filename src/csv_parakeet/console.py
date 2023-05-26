823a36988f9ae4eacc1f3939382b8ae87656cd25
Alfie Bowman <alfiebowman@protonmail.com>
2023-05-26 16:00:38 +0100

    Normalise filetype names

--- src/csv_parakeet/console.py
+++ src/csv_parakeet/console.py
@@ -7,7 +7,7 @@ from . import __version__
 @click.group()
 @click.version_option(version=__version__)
 def parakeet() -> None:
-    """Tool to easily convert between CSV and parquet files"""
+    """Tool to easily convert between CSV and Parquet files"""
     return None  # pragma: no cover
 
 
@@ -15,13 +15,13 @@ def parakeet() -> None:
 @click.argument("file_in", type=click.Path(exists=True), nargs=-1)
 @click.argument("file_out", type=click.Path(), nargs=1)
 def c2p(file_in, file_out):
-    """Convert a CSV file to parquet format
+    """Convert a CSV file to Parquet format
 
     \b
     Arguments:
-        FILE_IN: Path to csv files. If more than one path is given the CSV files are
+        FILE_IN: Path to CSV files. If more than one path is given the CSV files are
         concatenated together.
-        FILE_OUT: Desired path for parquet output
+        FILE_OUT: Desired path for Parquet output
     """
     pd.concat([pd.read_csv(path) for path in file_in], ignore_index=True).to_parquet(
         path=file_out, index=False
@@ -32,11 +32,11 @@ def c2p(file_in, file_out):
 @click.argument("file_in", type=click.Path(exists=True), nargs=1)
 @click.argument("file_out", type=click.Path(), nargs=1)
 def p2c(file_in, file_out):
-    """Convert a parqet file to CSV format
+    """Convert a Parquet file to CSV format
 
     \b
     Arguments:
-        FILE_IN: path to a parquet file
-        FILE_OUT: desired path for csv output
+        FILE_IN: Path to a Parquet file
+        FILE_OUT: Desired path for CSV output
     """
     pd.read_parquet(file_in).to_csv(path_or_buf=file_out, index=False)
